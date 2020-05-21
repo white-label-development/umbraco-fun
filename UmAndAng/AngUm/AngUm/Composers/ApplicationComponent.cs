27e0ebc21def72b3841b41d7b818a0686460f1eb
Neil Thompson <neil@white-label-development.co.uk>
2020-05-21 11:28:08 +0100

    default install and custom route for app

---
+++ UmAndAng/AngUm/AngUm/Composers/ApplicationComponent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Core.Composing;
+
+namespace AngUm.Composers
+{
+	// https://our.umbraco.com/Documentation/Reference/Events/Application-Startup
+	public class ApplicationComposer : ComponentComposer<ApplicationComponent>, IUserComposer
+	{
+		public override void Compose(Composition composition)
+		{
+			// ApplicationStarting event in V7: add IContentFinders, register custom services and more here
+
+			base.Compose(composition);
+		}
+	}
+
+	public class ApplicationComponent : IComponent
+	{
+		public void Initialize()
+		{
+            // ApplicationStarted event in V7: add your events here
+            RouteTable.Routes.MapRoute(
+              "app",
+              "app/{*.}",
+              new { controller = "AngularApp", action = "AngularAppView" });
+        }
+
+		public void Terminate()
+		{ }
+	}
+}
---
+++ UmAndAng/AngUm/AngUm/Controllers/AngularAppController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AngUm.Controllers
+{
+    public class AngularAppController : Umbraco.Web.Mvc.RenderMvcController
+    {
+        public ActionResult AngularAppView()
+        {
+            return View();
+        }
+
+        public ActionResult Hello()
+        {
+            return View();
+        }
+    }
+}
